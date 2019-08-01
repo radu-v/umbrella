fbc1044e65b640abf4469a3bdfe2d7ae998ace4f
Sultan Alsawaf <sultan@kerneltoast.com>
2019-07-31 18:02:49 -0700

    ion: system_heap: Speed up system heap allocations
    The system heap allocation process consists of allocating numerous
    temporary buffers that can instead be placed on the stack, to an extent;
    if the new 4 KiB on-stack buffer runs out, page_info allocations will
    fall back to using kmalloc.
    
    Additionally, since system heap allocations are frequent, they can
    benefit from the use of a memory pool for allocating the persistent
    sg_table structures. These allocations, along with a few others, also
    don't need to be zeroed out.
    
    These changes improve system heap allocation performance considerably.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/staging/android/ion/ion_system_heap.c
+++ drivers/staging/android/ion/ion_system_heap.c
@@ -354,6 +354,13 @@ static int ion_system_heap_allocate(struct ion_heap *heap,
 	struct device *dev = heap->priv;
 	struct page_info info_onstack[SZ_4K / sizeof(struct page_info)];
 
+	if (ion_heap_is_system_heap_type(buffer->heap->type) &&
+	    is_secure_vmid_valid(vmid)) {
+		pr_info("%s: System heap doesn't support secure allocations\n",
+			__func__);
+		return -EINVAL;
+	}
+
 	if (align > PAGE_SIZE)
 		return -EINVAL;
 
@@ -441,8 +448,7 @@ static int ion_system_heap_allocate(struct ion_heap *heap,
 				i = process_info(info, sg, sg_sync, &data, i);
 				free_info(info, info_onstack,
 					  ARRAY_SIZE(info_onstack));
-				if (sg_sync)
-					sg_sync = sg_next(sg_sync);
+				sg_sync = sg_next(sg_sync);
 			} else {
 				i = process_info(tmp_info, sg, 0, 0, i);
 				free_info(tmp_info, info_onstack,
@@ -451,8 +457,7 @@ static int ion_system_heap_allocate(struct ion_heap *heap,
 		} else if (info) {
 			i = process_info(info, sg, sg_sync, &data, i);
 			free_info(info, info_onstack, ARRAY_SIZE(info_onstack));
-			if (sg_sync)
-				sg_sync = sg_next(sg_sync);
+			sg_sync = sg_next(sg_sync);
 		} else if (tmp_info) {
 			i = process_info(tmp_info, sg, 0, 0, i);
 			free_info(tmp_info, info_onstack,
