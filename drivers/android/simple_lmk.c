8ffa3a21c5fece10e64ca98de701329d27b00e03
Sultan Alsawaf <sultan@kerneltoast.com>
2020-02-08 14:26:31 -0800

    simple_lmk: Place victims onto SCHED_RR
    Just increasing the victim's priority to the maximum niceness isn't
    enough to make it totally preempt everything in SCHED_FAIR, which is
    important to make sure victims die quickly. Resource-wise, this isn't
    very burdensome since the RT priority is just set to zero, and because
    dying victims don't have much to do: they only need to finish whatever
    they're doing quickly. SCHED_RR is used over SCHED_FIFO so that CPU time
    between the victims is divided evenly to help them all finish at around
    the same time, as fast as possible.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -198,6 +198,7 @@ static void scan_and_kill(unsigned long pages_needed)
 
 	/* Kill the victims */
 	for (i = 0; i < nr_to_kill; i++) {
+		static const struct sched_param sched_zero_prio;
 		struct victim_info *victim = &victims[i];
 		struct task_struct *vtsk = victim->tsk;
 
@@ -208,8 +209,8 @@ static void scan_and_kill(unsigned long pages_needed)
 		/* Accelerate the victim's death by forcing the kill signal */
 		do_send_sig_info(SIGKILL, SEND_SIG_FORCED, vtsk, true);
 
-		/* Increase the victim's priority to make it die faster */
-		set_user_nice(vtsk, MIN_NICE);
+		/* Elevate the victim to SCHED_RR with zero RT priority */
+		sched_setscheduler_nocheck(vtsk, SCHED_RR, &sched_zero_prio);
 
 		/* Allow the victim to run on any CPU. This won't schedule. */
 		set_cpus_allowed_ptr(vtsk, cpu_all_mask);
