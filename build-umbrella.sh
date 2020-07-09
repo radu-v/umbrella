8d39250e7894d5ec2dcd231854060c4d5fc660c2
Radu Vasilascu <glx33807@gmail.com>
2020-07-09 20:43:06 +0100

    Build script and fix build

---
+++ build-umbrella.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# this uses the Proton-CLang toolchain
+# https://github.com/kdrag0n/proton-clang.git
+
+echo
+echo "Cleanup output dir"
+echo
+
+rm -rf out
+make clean && make mrproper
+mkdir -p out
+
+echo
+echo "Build kernel"
+echo
+
+export PATH="$HOME/proton-clang/bin:$PATH"
+export LD_LIBRARY_PATH="$HOME/proton-clang/lib:$LD_LIBRARY_PATH"
+export ARCH=arm64
+export SUBARCH=arm64
+export AR=llvm-ar
+export NM=llvm-nm
+export OBJCOPY=llvm-objcopy
+export OBJDUMP=llvm-objdump
+export STRIP=llvm-strip
+
+#Make config
+make O=out ARCH=arm64 nb1_defconfig
+
+#Build kernel
+make -j$(nproc --all) O=out \
+    ARCH=arm64 \
+    CC="ccache clang" \
+    CROSS_COMPILE=aarch64-linux-gnu- \
+    CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+    LOCALVERSION=-clang-$(date +'%Y-%m-%d')
