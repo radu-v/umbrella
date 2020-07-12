c212c61b45fc87f4b1e1cd69c7764ba7a0477bda
Radu Vasilascu <glx33807@gmail.com>
2020-07-12 15:13:20 +0100

    clang: fix warnings (part 1)

--- sound/soc/msm/qdsp6v2/q6voice.c
+++ sound/soc/msm/qdsp6v2/q6voice.c
@@ -7112,7 +7112,7 @@ static int32_t qdsp_cvp_callback(struct apr_client_data *data, void *priv)
 				break;
 			}
 		}
-	} else if (data->opcode == VSS_ICOMMON_RSP_GET_PARAM ||
+	} else if (data->opcode == VSS_ICOMMON_RSP_GET_PARAM |
 		   VSS_ICOMMON_RSP_GET_PARAM_V3) {
 		pr_debug("%s: VSS_ICOMMON_RSP_GET_PARAM\n", __func__);
 		ptr = data->payload;
