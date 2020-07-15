f1a99bed0bd2fd9d932c50912e55285fca0e82ff
Radu Vasilascu <glx33807@gmail.com>
2020-07-15 15:08:31 +0100

    build-umbrella: remove args to disable warnings

--- build-umbrella.sh
+++ build-umbrella.sh
@@ -4,6 +4,16 @@
 # https://github.com/kdrag0n/proton-clang.git
 # when building under WSL make sure the distro uses WSL 2
 
+export PATH="$HOME/proton-clang/bin:$PATH"
+export LD_LIBRARY_PATH="$HOME/proton-clang/lib:$LD_LIBRARY_PATH"
+export ARCH=arm64
+export SUBARCH=arm64
+export AR=llvm-ar
+export NM=llvm-nm
+export OBJCOPY=llvm-objcopy
+export OBJDUMP=llvm-objdump
+export STRIP=llvm-strip
+
 echo
 echo "Cleanup output dir"
 echo
@@ -16,23 +26,13 @@ echo
 echo "Build kernel"
 echo
 
-export PATH="$HOME/proton-clang/bin:$PATH"
-export LD_LIBRARY_PATH="$HOME/proton-clang/lib:$LD_LIBRARY_PATH"
-export ARCH=arm64
-export SUBARCH=arm64
-export AR=llvm-ar
-export NM=llvm-nm
-export OBJCOPY=llvm-objcopy
-export OBJDUMP=llvm-objdump
-export STRIP=llvm-strip
-
 #Make config
 make O=out ARCH=arm64 nb1_defconfig
 
 #Build kernel
 make -j$(nproc --all) O=out CONFIG_DEBUG_SECTION_MISMATCH=y \
     ARCH=arm64 \
-    CC="ccache clang -Wno-enum-conversion -Wno-literal-conversion -Wno-section -Wno-strlcpy-strlcat-size -Wno-pointer-bool-conversion -Wno-self-assign" \
+    CC="ccache clang" \
     CROSS_COMPILE=aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
     LOCALVERSION=-clang-$(date +'%Y-%m-%d')
