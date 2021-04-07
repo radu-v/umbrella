fe0f54fff1a0d6ede15d832d93d215ae788dc2d0
Radu Vasilascu <glx33807@gmail.com>
2021-04-07 10:54:55 +0100

    ramdisk: disable vbswap init
    since vbswap is disabled anyway

--- umod/template/ramdisk/overlay.d/sbin/init.umod.post_boot.sh
+++ umod/template/ramdisk/overlay.d/sbin/init.umod.post_boot.sh
@@ -2,18 +2,18 @@
 
 if [ ! -f /sbin/recovery ]; then
   # Setup swap
-  while [ ! -e /dev/block/vbswap0 ]; do
-    sleep 1
-  done
-  if ! grep -q vbswap /proc/swaps; then
-    # 1GB
-    echo 1073741824 > /sys/devices/virtual/block/vbswap0/disksize
-    echo 160 > /proc/sys/vm/swappiness
+#  while [ ! -e /dev/block/vbswap0 ]; do
+#    sleep 1
+#  done
+#  if ! grep -q vbswap /proc/swaps; then
+#    # 1GB
+#    echo 1073741824 > /sys/devices/virtual/block/vbswap0/disksize
+#    echo 160 > /proc/sys/vm/swappiness
 
-    mkswap /dev/block/vbswap0
-    swapon /dev/block/vbswap0
-    echo "umod: vbswap initialized" > /dev/kmsg
-  fi
+#    mkswap /dev/block/vbswap0
+#    swapon /dev/block/vbswap0
+#    echo "umod: vbswap initialized" > /dev/kmsg
+#  fi
 fi
 
 # Remove unused swapfile
