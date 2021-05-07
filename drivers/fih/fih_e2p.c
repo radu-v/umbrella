b2a523757419d8e33623cc808501aae99ba69039
Radu Vasilascu <glx33807@gmail.com>
2021-05-07 19:27:11 +0100

    drivers: fih_e2p: move fih_sensordata to fih_e2p
    since the sensor HAL is looking for /proc/sensordata/ALS,
    create the endpoint
    
    Signed-off-by: Radu Vasilascu <glx33807@gmail.com>

--- drivers/fih/fih_e2p.c
+++ drivers/fih/fih_e2p.c
@@ -3,6 +3,7 @@
 #include <linux/string.h>
 #include <linux/proc_fs.h>
 #include <linux/io.h>
+#include <linux/slab.h>
 #include <fih/share/e2p.h>
 #include <linux/seq_file.h>
 #include "fih_ramtable.h"
@@ -97,6 +98,35 @@ static int fih_e2p_proc_read_wifi_mac2(struct inode *inode, struct file *file)
 	return single_open(file, fih_e2p_proc_read_wifi_mac2_show, NULL);
 }
 
+static int fih_sensordata2_show(struct seq_file *m, void *v)
+{
+	void *io_block;
+	void *block = kzalloc(FIH_SENSORDATA2_SIZE, GFP_KERNEL);
+
+	if (!block) {
+		pr_err("%s: could not allocate buffer", __func__);
+		return 0;
+	}
+
+	io_block = ioremap(FIH_SENSORDATA2_BASE, FIH_SENSORDATA2_SIZE);
+	if (io_block == NULL) {
+		pr_err("%s: ioremap fail", __func__);
+		return (0);
+	}
+
+	memcpy_fromio(block, io_block, FIH_SENSORDATA2_SIZE);
+	iounmap(io_block);
+
+	seq_write(m, block, FIH_SENSORDATA2_SIZE);
+	kfree(block);
+
+	return 0;
+}
+
+static int fih_sensordata2(struct inode *inode, struct file *file)
+{
+	return single_open(file, fih_sensordata2_show, NULL);
+}
 
 /* This structure gather "function" that manage the /proc file
  */
@@ -122,10 +152,18 @@ static const struct file_operations wifi_mac2_file_ops = {
 	.open    = fih_e2p_proc_read_wifi_mac2,
 	.read    = seq_read
 };
-
+static const struct file_operations fih_sensordata2_file_ops = {
+	.owner		= THIS_MODULE,
+	.open		= fih_sensordata2,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= single_release,
+};
 
 static int __init fih_e2p_init(void)
 {
+	struct proc_dir_entry *root;
+
 	if (FIH_E2P_ST_SIZE < sizeof(FIH_E2P_DATA_ST)) {
 		pr_err("%s: WARNNING!! FIH_E2P_ST_SIZE (%d) < sizeof(FIH_E2P_DATA_ST) (%lu)",
 			__func__, FIH_E2P_ST_SIZE, sizeof(FIH_E2P_DATA_ST));
@@ -146,6 +184,14 @@ static int __init fih_e2p_init(void)
 
 	if (proc_create("wifi_mac2", 0, NULL, &wifi_mac2_file_ops) == NULL) {
 		pr_err("fail to create proc/wifi_mac\n");
+	}
+
+	root = proc_mkdir("sensordata", NULL);
+	if (!root)
+		return 1;
+
+	if (proc_create("sensordata/ALS", 0, NULL, &fih_sensordata2_file_ops) == NULL) {
+		pr_err("fail to create proc/sensordata/ALS");
 	}
 
 	return (0);
